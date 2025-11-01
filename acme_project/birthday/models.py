c621a1582f40402e85fcbd1ddc079712f516a613
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-11-01 18:06:01 +0200

    tags

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -6,6 +6,13 @@ from .validators import real_age
 
 User = get_user_model()
 
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    # Переопределяем метод:
+    def __str__(self):
+        return self.tag
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
@@ -17,6 +24,12 @@ class Birthday(models.Model):
     # Валидатор указывается в описании поля.
     birthday = models.DateField('Дата рождения', validators=(real_age,))
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
+    ) 
 
     class Meta:
         constraints = (
